ccc25aadb14456736a331a6000c96a8c8268c842
siam <samisiam851@gmail.com>
2023-11-10 23:24:46 +0600

    post slice

--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -6,7 +6,12 @@ const postSlice = createSlice({
     { id: 1, title: 'Hey html', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem accusantium sunt, non ab reiciendis incidunt, adipisci aliquam asperiores tenetur eaque voluptatem nostrum similique numquam beatae inventore repellat consectetur laudantium eligendi' },
     { id: 2, title: 'Hey html5', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem accusantium sunt, non ab reiciendis incidunt, adipisci aliquam asperiores tenetur eaque voluptatem nostrum similique numquam beatae inventore repellat consectetur laudantium eligendi' },
   ],
-  reducers: {}
+  reducers: {
+
+    postAdded  (state, action) {
+state.push(action.payload)
+    }
+  }
 });
 
 
